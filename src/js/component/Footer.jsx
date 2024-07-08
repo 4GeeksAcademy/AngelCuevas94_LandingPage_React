c7918dc25d394849f6492fa7948c1759b7545a13
AngelCS94 <163451592+AngelCS94@users.noreply.github.com>
2024-07-08 18:24:46 +0000

    [Update: Home.jsx] Creados Componentes 1.Card 2.Footer 3.Jumbotron 4.Navbar

---
+++ src/js/component/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Footer = ()=>{
+
+return (
+
+<nav className="navbar fixed-bottom navbar-light bg-light">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">Fixed bottom</a>
+  </div>
+</nav>
+);
+
+}
+
+export default Footer;
