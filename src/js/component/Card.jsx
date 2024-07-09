c0e8620ff69a6806ab3bc2ebb8e6dfc9e878c0f3
AngelCS94 <163451592+AngelCS94@users.noreply.github.com>
2024-07-09 16:48:57 +0000

    por retocar

--- src/js/component/Card.jsx
+++ src/js/component/Card.jsx
@@ -7,7 +7,7 @@ const Card = () => {
             <img src="https://images.unsplash.com/photo-1720044337614-467ae9e0e23b?q=80&w=3174&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className="card-img-top" alt="paisaje"/>
                 <div className="card-body">
                     <h5 className="card-title">Card title</h5>
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <p className="card-text">Bacon ipsum dolor amet andouille biltong pork pancetta.</p>
                     <a href="#" className="btn btn-primary">Go somewhere</a>
                 </div>
         </div>
