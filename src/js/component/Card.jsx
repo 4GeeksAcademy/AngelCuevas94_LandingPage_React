2927b62bf9b1fcfc3507b808e1c2f493b1734879
AngelCS94 <163451592+AngelCS94@users.noreply.github.com>
2024-07-10 19:16:30 +0000

    [Update: Añadidos props]

--- src/js/component/Card.jsx
+++ src/js/component/Card.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
-const Card = () => {
+const Card = ({title, url,texto}) => {
 
     return (
         <div className="card text-center">
-            <img src="https://images.unsplash.com/photo-1720044337614-467ae9e0e23b?q=80&w=3174&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className="card-img-top" alt="paisaje"/>
+            <img src={url} className="card-img-top" style={{objectFit:"fill", width:"200px", height:"130px", justifyContent:"center"}} alt="paisaje"/>
                 <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">Bacon ipsum dolor amet andouille biltong pork pancetta.</p>
+                    <h5 className="card-title">{title}</h5>
+                    <p className="card-text">{texto}</p>
                     <a href="#" className="btn btn-primary">Go somewhere</a>
                 </div>
         </div>
