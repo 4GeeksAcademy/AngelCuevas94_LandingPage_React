57a52354962850550df4434fe71f487cfc662153
AngelCS94 <163451592+AngelCS94@users.noreply.github.com>
2024-07-10 17:17:44 +0000

    [Updated: SRC/ Componentes] Navbar.jsx: Color Jumbotron.jsx: Color de fondo y bordes. Añadido boton y texto Footer.jsx: Cambio de color, centrado de texto y añadido nuevi texto.

--- src/js/component/Footer.jsx
+++ src/js/component/Footer.jsx
@@ -4,9 +4,9 @@ const Footer = ()=>{
 
 return (
 
-<nav className="navbar bottom navbar-light bg-light" style={{marginTop:"2rem"}}>
+<nav className="navbar bottom navbar-light bg-dark" style={{marginTop:"2rem"}}>
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">Fixed bottom</a>
+    <a className="navbar-brand text-white" style={{marginLeft:"auto", marginRight:"auto", fontSize:"small"}} href="#">Copyrigth @ YourWebsite 2024</a>
   </div>
 </nav>
 );
--- src/js/component/Jumbotron.jsx
+++ src/js/component/Jumbotron.jsx
@@ -3,11 +3,11 @@ import React from "react";
 const Jumbotron = () => {
 
     return (
-        <div className="card col-lg-11 mt-3 border-0 bg-secondary" style={{height:"15rem", width:"-30px", marginLeft:"auto",marginRight:"auto"}}>
+        <div className="card col-lg-11 mt-3 border-0 bg-light" style={{height:"15rem", width:"-30px", marginLeft:"auto",marginRight:"auto"}}>
             <div className="card-body">
-                <h1 className="card-title d-flex justify-content-start">A Warm Welcome!</h1>
-                <p className="card-text d-flex justify-content-start">With supporting text below as a natural lead-in to additional content.</p>
-                
+                <h1 className="card-title d-flex justify-content-start mt-2">A Warm Welcome!</h1>
+                <p className="card-text d-flex justify-content-start mt-3">Bacon ipsum dolor amet chicken bacon t-bone corned beef capicola short loin rump pork pig. Tail shankle ribeye, andouille bresaola tenderloin swine spare ribs brisket chicken burgdoggen hamburger. </p>
+                <a href="#" class="btn btn-primary mt-3">Call to action</a>
             </div>
         </div>
     );
